c0abea989666dd6c6c02487ea298e6175a3db19f
jasonheesanglee <jason.heesang.lee96@gmail.com>
2023-05-01 14:42:46 +0900

    feat: Done get_lucky_nums():

--- lucky_number.py
+++ lucky_number.py
@@ -1,9 +1,11 @@
 import random as rd
-
+ 
 def get_lucky_nums():
   return rd.sample(range(1, 45+1), k=6)
 
 if __name__ == '__main__':
-  print(get_lucky_nums())
+  times = int(input('enter numbers from 1-100 : '))
+  for _ in range(times):
+    print(get_lucky_nums())
 
 
