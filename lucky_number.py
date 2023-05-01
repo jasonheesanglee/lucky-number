11d2db7961c086fff1dccf433b1ec34ec77de193
jasonheesanglee <jason.heesang.lee96@gmail.com>
2023-05-01 14:36:18 +0900

    feat: updated .py file
    added few more lines of code.

--- lucky_number.py
+++ lucky_number.py
@@ -4,8 +4,12 @@ counter = 6
 lotto = []
 
 for i in range(6):
-  selected_number = rd.randint(6, 45)
+  selected_number = rd.randint(1, 45+1)
   lotto.append(selected_number)
 
 print(lotto)
+
+result = rd.sample(range(1, 45+1), k=6)
+print(result)
+
 
